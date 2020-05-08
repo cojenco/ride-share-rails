454f1a93483ff8bdc35e8eee8977272320983625
cojenco <icathyouyang@gmail.com>
2020-05-08 08:59:19 -0700

    add passengers#index #show actions and ERB views

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,15 @@
 class PassengersController < ApplicationController
+  def index
+    @passengers = Passenger.all
+  end
+
+  def show
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+  end
+
 end
