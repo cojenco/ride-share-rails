1a506e53db218ad49ca9b1a7580ccfc4c72923d3
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-07 23:28:38 -0700

    add show in drivers contiller and add view

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -5,9 +5,11 @@ class DriversController < ApplicationController
   end
 
   def show
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+      return
+    end
   end
-  
-
-
   
 end
