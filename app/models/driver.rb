03478267eb11acf42487d3bb4f8db1f1060bfa7e
cojenco <icathyouyang@gmail.com>
2020-05-09 12:36:31 -0700

    add avg_rating method to Driver Model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,11 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def avg_rating
+    valid_trips = self.trips.where.not(rating: nil)
+    total_ratings = valid_trips.map {|trip| trip.rating}
+    avg_rating = total_ratings.sum.to_f  / valid_trips.count
+    return avg_rating
+  end
+
 end
