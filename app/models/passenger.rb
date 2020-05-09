6c2659b879016f8610e6f3cc8f0a68d1b0e1d1f2
cojenco <icathyouyang@gmail.com>
2020-05-09 13:51:54 -0700

    add total_charges for passengers and total_trips for drivers

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,9 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def total_charges
+    valid_trips = self.trips.where.not(cost: nil)
+    total_charges = valid_trips.map {|trip| trip.cost}
+    return total_charges.sum.round(2)
+  end
 end
