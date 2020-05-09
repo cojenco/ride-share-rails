592149637c91c7e14ad6bd5d068800c57bbf171e
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-08 17:01:05 -0700

    add edit action to trip controller and add view

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -8,7 +8,15 @@ class TripsController < ApplicationController
     end
   end
 
-  
+  def edit
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.nil?
+      head :not_found
+      return
+    end
+  end
+
+
 
 
 
