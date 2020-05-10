2a659a83348627566e6482b57c71f5ed76a53f07
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-09 20:37:04 -0700

    add test for passenger contorller index and show

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -2,11 +2,33 @@ require "test_helper"
 
 describe PassengersController do
   describe "index" do
-    # Your tests go here
+    it "responds with success when there are many passenger saved" do
+      get "/passengers"
+      must_respond_with :success
+    end
+
+    # it "responds with success when there are no passenger" do
+    #   get "/passengers"
+    #   must_respond_with :not_found
+    # end
   end
 
   describe "show" do
-    # Your tests go here
+    before do
+      @passenger = Passenger.create(name: "Cathy Something")
+    end
+
+    it "will get show for valid id" do
+      valid_passanger_id = @passenger.id
+      get "/passengers/#{valid_passanger_id}"
+      must_respond_with :success
+    end
+
+    it "will respond with not_found for invalid ids" do
+      invalid_passanger_id = 99999999999999
+      get "/passengers/#{invalid_passanger_id}"
+      must_respond_with :not_found
+    end
   end
 
   describe "new" do
